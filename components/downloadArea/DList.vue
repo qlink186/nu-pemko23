98b5d594812a744f8ca480c17899cc5292defd2e
qlink186 <qlink186@ymail.com>
2023-05-26 15:04:57 +0700

    230526

--- components/downloadArea/DList.vue
+++ components/downloadArea/DList.vue
@@ -1,25 +1,79 @@
 <template>
-  <ul class="list-group list-group-flush lst_down">
+  <ul class="list-group lst_down">
     <li class="list-group-item list-group-item-action d-flex align-items-center lst_down_item">
       <div class="me-auto d-flex align-items-center lst_down_item_desk">
         <div class="lst_down_item_desk_icon"><img src="~/assets/img/icon/format/pdf.svg" class="img-fluid mx-auto d-block"
             alt="format"></div>
-        <div class="lst_down_item_desk_konten">
+        <div class="lst_down_item_desk_konten ps-2">
+          <div class="lst_down_item_desk_konten_judul">Pengumuman Kelulusan Seleksi Administrasi Pemilihan Calon Anggota Badan Penyelesaian Sengketa Konsumen (BPSK) Kota Tanjungpinang Periode Tahun 2023 - 2028</div>
+          <div class="lst_down_item_desk_konten_desk">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores
+            esse dicta qui natus, mollitia earum amet debitis perferendis autem sequi perspiciatis nisi? Dolor ipsa eaque
+            veniam magni iste animi molestias.</div>
+          <div class="lst_down_item_desk_konten_meta">
+            <span class="pe-3 lst_down_item_desk_konten_meta_opd"><Icon name="ri:government-fill" class="pe-1" />Diskominfo</span>
+            <span class="pe-3 lst_down_item_desk_konten_meta_jnsdok"><Icon name="icon-park-twotone:view-grid-detail" class="pe-1" />Pengumuman</span>
+            <span class="pe-3 lst_down_item_desk_konten_meta_jnsinf"><Icon name="ic:twotone-view-day" class="pe-1" />Serta Merta</span>
+          </div>
+        </div>
+      </div>
+      <div class="align-middle text-center lst_down_item_tgl">22 Desember 2023</div>
+      <div class="lst_down_item_aksi d-grid gap-1 d-md-flex justify-content-md-end">
+        <button class="btn btn-primary btn-sm" type="button">
+          <Icon name="line-md:download-loop" />
+        </button>
+        <button class="btn btn-warning btn-sm" type="button">
+          <Icon name="ph:magnifying-glass-plus-bold"/>
+        </button>
+      </div>
+    </li>
+    <li class="list-group-item list-group-item-action d-flex align-items-center lst_down_item">
+      <div class="me-auto d-flex align-items-center lst_down_item_desk">
+        <div class="lst_down_item_desk_icon"><img src="~/assets/img/icon/format/doc.svg" class="img-fluid mx-auto d-block"
+            alt="format"></div>
+        <div class="lst_down_item_desk_konten ps-2">
+          <div class="lst_down_item_desk_konten_judul">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</div>
+          <div class="lst_down_item_desk_konten_desk">Lorem ipsum dolor sit,</div>
+          <div class="lst_down_item_desk_konten_meta">
+            <span class="pe-3 lst_down_item_desk_konten_meta_opd"><Icon name="ri:government-fill" class="pe-1" />Diskominfo</span>
+            <span class="pe-3 lst_down_item_desk_konten_meta_jnsdok"><Icon name="icon-park-twotone:view-grid-detail" class="pe-1" />Pengumuman</span>
+            <span class="pe-3 lst_down_item_desk_konten_meta_jnsinf"><Icon name="ic:twotone-view-day" class="pe-1" />Serta Merta</span>
+          </div>
+        </div>
+      </div>
+      <div class="align-middle text-center lst_down_item_tgl">22 Desember 2023</div>
+      <div class="lst_down_item_aksi d-grid gap-1 d-md-flex justify-content-md-end">
+        <button class="btn btn-primary btn-sm" type="button">
+          <Icon name="line-md:download-loop" />
+        </button>
+        <button class="btn btn-warning btn-sm" type="button">
+          <Icon name="ph:magnifying-glass-plus-bold"/>
+        </button>
+      </div>
+    </li>
+    <li class="list-group-item list-group-item-action d-flex align-items-center lst_down_item">
+      <div class="me-auto d-flex align-items-center lst_down_item_desk">
+        <div class="lst_down_item_desk_icon"><img src="~/assets/img/icon/format/zip.svg" class="img-fluid mx-auto d-block"
+            alt="format"></div>
+        <div class="lst_down_item_desk_konten ps-2">
           <div class="lst_down_item_desk_konten_judul">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</div>
           <div class="lst_down_item_desk_konten_desk">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores
             esse dicta qui natus, mollitia earum amet debitis perferendis autem sequi perspiciatis nisi? Dolor ipsa eaque
             veniam magni iste animi molestias.</div>
           <div class="lst_down_item_desk_konten_meta">
-            <span class="pe-3 lst_down_item_desk_konten_meta_opd">Diskominfo</span>
-            <span class="pe-3 lst_down_item_desk_konten_meta_jnsdok">Pengumuman</span>
-            <span class="pe-3 lst_down_item_desk_konten_meta_jnsinf">Serta Merta</span>
+            <span class="pe-3 lst_down_item_desk_konten_meta_opd"><Icon name="ri:government-fill" class="pe-1" />Diskominfo</span>
+            <span class="pe-3 lst_down_item_desk_konten_meta_jnsdok"><Icon name="icon-park-twotone:view-grid-detail" class="pe-1" />Pengumuman</span>
+            <span class="pe-3 lst_down_item_desk_konten_meta_jnsinf"><Icon name="ic:twotone-view-day" class="pe-1" />Serta Merta</span>
           </div>
         </div>
       </div>
       <div class="align-middle text-center lst_down_item_tgl">22 Desember 2023</div>
-      <div class="d-grid gap-2 lst_down_item_aksi">
-        <button class="btn btn-primary btn-sm" type="button">Download</button>
-        <button class="btn btn-warning btn-sm" type="button">Selengkapnya</button>
+      <div class="lst_down_item_aksi d-grid gap-1 d-md-flex justify-content-md-end">
+        <button class="btn btn-primary btn-sm" type="button">
+          <Icon name="line-md:download-loop" />
+        </button>
+        <button class="btn btn-warning btn-sm" type="button">
+          <Icon name="ph:magnifying-glass-plus-bold"/>
+        </button>
       </div>
     </li>
   </ul>
@@ -44,24 +98,39 @@ $warnasecondary: $oren;
 .lst {
   &_down {
     &_item {
-      background-color: $abumuda4;
+      background-color: lighten($abumuda4,2);
       border-radius: .5rem;
       border: 1px solid $abumuda3;
+      margin-bottom: 1rem;
+      padding: .75rem;
+      transition: all .5s ease-out;
+      -moz-transition: all .5s ease-out;
+      -webkit-transition: all .5s ease-out;
+      -o-transition: all .5s ease-out;
       &_desk {
         &_icon {
-          min-width: 4rem;
+          min-width: 3.5rem;
 
           img {
-            width: 60px;
+            width: 50px;
+            filter: grayscale(1);
+            transition: all .5s ease-out;
+            -moz-transition: all .5s ease-out;
+            -webkit-transition: all .5s ease-out;
+            -o-transition: all .5s ease-out;
           }
         }
 
         &_konten {
-          max-width: 31rem;
+          max-width: 30rem;
 
           &_judul {
             font-size: .8rem;
             font-weight: 600;
+            transition: all .5s ease-out;
+            -moz-transition: all .5s ease-out;
+            -webkit-transition: all .5s ease-out;
+            -o-transition: all .5s ease-out;
           }
 
           &_desk {
@@ -71,12 +140,31 @@ $warnasecondary: $oren;
 
           &_meta {
             color: $abumuda2;
-            font-size: .65rem;
-            &_opd {}
+            font-size: .7rem;
+            transition: all .5s ease-out;
+            -moz-transition: all .5s ease-out;
+            -webkit-transition: all .5s ease-out;
+            -o-transition: all .5s ease-out;
+            &_opd,
+            &_jnsdok,
+            &_jnsinf{
+              transition: all .5s ease-out;
+              -moz-transition: all .5s ease-out;
+              -webkit-transition: all .5s ease-out;
+              -o-transition: all .5s ease-out;
+              .icon{
+                font-size: .95rem;
+                margin-top: -2px;
+              }
+            }
+            &_opd {
+            }
 
-            &_jnsdok {}
+            &_jnsdok {
+            }
 
-            &_jnsinf {}
+            &_jnsinf {
+            }
           }
         }
       }
@@ -87,20 +175,33 @@ $warnasecondary: $oren;
         width: 10rem;
       }
 
-      &_aksi {}
+      &_aksi {
+        width: 4.7rem;
+        .btn{
+          font-size: 1.1rem;
+          padding: 0 .3rem;
+          border-radius: .4rem;
+        }
+      }
 
       &:hover {
+        background-color: inherit;
+        border-color: lighten($abumuda2,20);
+        box-shadow: 0 0 10px 0px $abumuda3;
+          -webkit-box-shadow: 0 0 10px 0px $abumuda3;
+          -moz-box-shadow: 0 0 10px 0px $abumuda3;
         .lst {
           &_down {
             &_item {
               &_desk {
 
                 &_icon {
-                  img {}
+                  img {
+                    filter: grayscale(0);
+                  }
                 }
 
                 &_konten {
-                  max-width: 31rem;
 
                   &_judul {
                     color: $warnadefault;
@@ -113,9 +214,13 @@ $warnasecondary: $oren;
                       color: $merah;
                     }
 
-                    &_jnsdok {}
+                    &_jnsdok {
+                      color: $hijau;
+                    }
 
-                    &_jnsinf {}
+                    &_jnsinf {
+                      color: $oren;
+                    }
                   }
                 }
               }
@@ -131,4 +236,5 @@ $warnasecondary: $oren;
       }
     }
   }
-}</style>
+}
+</style>

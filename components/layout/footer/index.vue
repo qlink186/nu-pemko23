ebd497909e5e1f60ec70ab2ac552cd9a600794b8
qlink186 <qlink186@ymail.com>
2023-07-03 11:33:28 +0700

    230703-footer

--- components/layout/footer/index.vue
+++ components/layout/footer/index.vue
@@ -1,12 +1,13 @@
 <template>
   <footer class="footer">
     <div class="waves">
+      <div class="gedungWako"><img src="~/assets/img/gedung_wako.png" class="img-fluid mx-auto d-block" alt="Gedung Wako"></div>
       <div class="wave" id="wave1"></div>
       <div class="wave" id="wave2"></div>
       <div class="wave" id="wave3"></div>
       <div class="wave" id="wave4"></div>
     </div>
-    <div class="container-fluid bgHitam1 py-4">
+    <div class="container-fluid bgBiru pb-4">
     <div class="container footer_atas">
       <div class="row">
         <div class="col-md-10 order-md-2">
@@ -32,7 +33,7 @@
       </div>
     </div>
   </div>
-  <div class="container-fluid bgHitam2">
+  <div class="container-fluid bgBiru2">
     <div class="container footer_bawah text-center">
       <p class="my-0">© 2023 <a href="https://kominfo.tanjungpinangkota.go.id/" target="_BLANK">DISKOMINFO</a> Kota Tanjungpinang</p>
     </div>

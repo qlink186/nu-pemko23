6e3a71bf7922a685275b55865300da0f0e11a048
qlink186 <qlink186@ymail.com>
2023-07-03 20:59:50 +0700

    230703-footer

--- composables/useNavbar.ts
+++ composables/useNavbar.ts
@@ -1,5 +1,8 @@
 export const useNavbar = () => {
   const config = useRuntimeConfig()
-  const { data: menu } = useAsyncData('menu', () => $fetch(config.public['apiUrl']+`menu`))
+  const { data: menu } = useAsyncData(
+    'menu', 
+    () => $fetch(config.public['apiUrl']+`menu`)
+  )
   return { menu }
 }
